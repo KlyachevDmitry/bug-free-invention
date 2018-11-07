56c23110430206720c8fec501f8dea0204191886
KlyachevDmitry <44836863+KlyachevDmitry@users.noreply.github.com>
2018-11-07 14:35:58 +0300

    Add files via upload

---
+++ Exel_write.py
@@ -0,0 +1,38 @@
+﻿import openpyxl
+import socket
+import sys
+
+#Создаем объект wb
+wb = openpyxl.load_workbook(filename = 'C:/Users/Дмитрий/AppData/Local/Programs/Python/Python36/testX.xlsx')
+
+#активируем лист в эксель
+ws = wb.active
+
+#Выбираем страницу
+sheet = wb['test']
+
+#считываем значения из ячеек
+vals = [sheet.cell(row=1,column=i).value for i in range(1,11)]
+
+#преобразуем массив в строку
+vals=str(vals)
+
+#создаем сокет "conn"
+conn = socket.socket()
+
+#подключаемся к машине
+conn.connect( ("192.168.1.102", 14900) )
+
+#декодируем строки в байты и отправляем 
+conn.send(b"Hello! \n")
+conn.send(vals.encode('utf-8'))
+
+while 1:
+    a=input("Commands: ")
+    conn.send(a.encode('utf-8'))
+    if (a=="exit"):
+        conn.send(b"exit")
+        break
+    a=b""
+conn.close()
+
---
+++ serverEx.py
@@ -0,0 +1,44 @@
+﻿import socket
+import openpyxl
+
+#Создаем сокет sock
+sock=socket.socket()
+
+#Для привязки используется функция bind сокета, которая принимает массив, содержащий два элемента: хост и порт.
+sock.bind(("",14900))
+
+#Слушаем соединения
+sock.listen(10)
+
+#принимаем соединения с помощью функции accept
+conn, addr=sock.accept()
+
+# функцией recv контролируем еколичество получаемых байт
+data=conn.recv(1000)
+udata=data.decode("utf-8")
+print(udata)
+
+#декодируем и отправялем данные
+exel=conn.recv(1024)
+uexel=exel.decode("utf-8")
+
+#закрываем сокет
+conn.close()
+
+#
+wb = openpyxl.load_workbook(filename = 'C:/Users/deman/AppData/Local/Programs/Python/Python36/Projects/testX.xlsx')
+#
+ws = wb.active
+
+#открываем страницу
+sheet = wb['test']
+uexel = eval(uexel)
+
+#записываем последовательность
+i = 1
+for rec in uexel:
+    rec=float(rec)
+    sheet.cell(row=1, column=i).value = rec
+    i += 1
+#сохраняем файл
+wb.save('C:/Users/deman/AppData/Local/Programs/Python/Python36/Projects/testX3.xlsx')
